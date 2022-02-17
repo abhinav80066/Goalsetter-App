ad1c5d66237b2dc5ea96e6c16e9d72483acab6b8
abhinav80066 <abhinav.chaudhary800@gmail.com>
2022-02-17 16:44:05 +0530

    created register user functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,7 +21,28 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('User already exist')
     }
-    res.json({ message: 'Register User'})
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid User Data')
+    }
 })
 
 //@description  Authenticate a user
