838cd4e90cba805d262efe8cf5ba679c413f9e5b
abhinav80066 <abhinav.chaudhary800@gmail.com>
2022-02-19 12:17:37 +0530

    Make the user routes protected

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -5,7 +5,7 @@ const Goal = require('../models/goalModel')
 //@route        Get /api/goals
 //@access       Private
 const getGoals = asyncHandler(async(req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
     res.status(200).json(goals)
 })
 
@@ -20,6 +20,7 @@ const setGoal = asyncHandler(async(req, res) => {
 
     const goal = await Goal.create({
         text: req.body.text,
+        user: req.user.id
     })
 
     res.status(200).json(goal)
