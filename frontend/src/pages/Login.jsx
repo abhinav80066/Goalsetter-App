c1c322c351362ec682e0c9cefdf6c19c720f59f9
abhinav80066 <abhinav.chaudhary800@gmail.com>
2022-02-20 16:45:01 +0530

    Added frontend UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,69 @@
-function Login() {
-  return (
-    <div>Login</div>
-  )
+import { useState, useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+
+function Register() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const { email, password } = formData
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]:e.target.value,
+        }))
+     }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <>
+            <section className="heading">
+                <h1>
+                    < FaSignInAlt /> Login
+                </h1>
+                <p> Login and start setting your Goals</p>
+            </section>
+            <section className='form'>
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input 
+                            type='email' 
+                            className='form-control' 
+                            id='email' 
+                            name='email' 
+                            value={email} 
+                            placeholder='Enter your email' 
+                            onChange={onChange} 
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <input 
+                            type='password' 
+                            className='form-control' 
+                            id='password' 
+                            name='password' 
+                            value={password} 
+                            placeholder='Enter password' 
+                            onChange={onChange} 
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <button type='submit' className='btn btn-block'>
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </section>
+
+        </>
+    )
 }
 
-export default Login
+export default Register
