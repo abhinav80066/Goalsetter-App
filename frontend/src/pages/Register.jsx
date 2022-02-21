e21b68ca8827c8a0eada21eb69d9dce9c3f6107a
abhinav80066 <abhinav.chaudhary800@gmail.com>
2022-02-21 16:53:58 +0530

    Register functionality working

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,6 +4,7 @@ import { FaUser } from 'react-icons/fa'
 import {useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
 import {register, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -18,6 +19,22 @@ function Register() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
     
+    const { user, isLoading, isError, isSuccess, message } = useSelector(
+        (state) => state.auth
+    )
+
+    useEffect(() => {
+        if(isError){
+            toast.error(message)
+        }
+
+        if(isSuccess || user){
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -28,6 +45,22 @@ function Register() {
 
     const onSubmit = (e) => {
         e.preventDefault()
+
+        if (password!==password2) {
+            toast.error('Passwords do not match')
+        } else {
+            const userData = {
+                name,
+                email,
+                password,
+            }
+
+            dispatch(register(userData))
+        }
+    }
+
+    if(isLoading) {
+        return <Spinner />
     }
 
     return (
