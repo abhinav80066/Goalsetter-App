95c536e1e6a96f92031c6a32dabeca03d0138785
abhinav80066 <abhinav.chaudhary800@gmail.com>
2022-02-22 16:53:51 +0530

    Dashboard only visible after login

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,18 @@
+import {useEffect} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 function Dashboard() {
+  const navigate = useNavigate()
+
+  const {user} = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if(!user) {
+      navigate('/login')
+    }
+  }, [user, navigate])
+  
   return (
     <div>Dashboard</div>
   )
