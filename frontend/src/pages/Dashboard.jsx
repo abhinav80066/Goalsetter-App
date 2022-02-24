1f8fb8c7ca19b2768f5120131cab67bec6867503
abhinav80066 <abhinav.chaudhary800@gmail.com>
2022-02-24 20:22:06 +0530

    Now we can see our goals in dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useSelector, useDispatch} from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
 import { getGoals, reset } from '../features/goals/goalSlice'
 
@@ -25,6 +26,10 @@ function Dashboard() {
     }
   }, [user, navigate, isError, message, dispatch])
 
+  if(isloading){
+    return <Spinner />
+  }
+
   return (
     <>
       <section className='heading'>
@@ -32,6 +37,16 @@ function Dashboard() {
         <p>Goals Dashboard</p>
       </section>
       <GoalForm />
+
+      <section className='content'>
+        {goals.length > 0 ? (
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key = {goal._id} goal={goal} />      
+            ))}
+          </div>
+        ) : (<h3> You have not set any goals </h3>)}
+      </section>
     </>
   )
 }
