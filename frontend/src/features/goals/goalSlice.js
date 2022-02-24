5358c907b8917ee48ef550d49f73dfa63e00897c
abhinav80066 <abhinav.chaudhary800@gmail.com>
2022-02-24 22:08:45 +0530

    Now we can delete the goals from dashboard

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -29,7 +29,6 @@ export const createGoal = createAsyncThunk(
 )
 
 // Get user goals
-
 export const getGoals = createAsyncThunk('goals/getAll', async(_, thunkAPI) => {
     try {
         const token = thunkAPI.getState().auth.user.token
@@ -44,6 +43,25 @@ export const getGoals = createAsyncThunk('goals/getAll', async(_, thunkAPI) => {
             return thunkAPI.rejectWithValue(message)
     }
 })
+
+// Delete user goal
+export const deleteGoal = createAsyncThunk(
+    'goals/delete',
+    async (id, thunkAPI) => {
+      try {
+        const token = thunkAPI.getState().auth.user.token
+        return await goalService.deleteGoal(id, token)
+      } catch (error) {
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString()
+        return thunkAPI.rejectWithValue(message)
+      }
+    }
+  )
 
 export const goalSlice = createSlice({
     name: 'goal',
@@ -79,6 +97,21 @@ export const goalSlice = createSlice({
                 state.isError = true
                 state.message = action.payload
             })
+            .addCase(deleteGoal.pending, (state) => {
+                state.isLoading = true
+              })
+              .addCase(deleteGoal.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.goals = state.goals.filter(
+                  (goal) => goal._id !== action.payload.id
+                )
+              })
+              .addCase(deleteGoal.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+              })
     }
 })
 
